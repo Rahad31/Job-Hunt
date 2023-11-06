cefb15bacca9d5579af62847fa3a85d39590b49c
Rahad31 <rahad3100@gmail.com>
2023-11-06 13:11:23 +0600

    Banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,9 +4,30 @@ const Banner = () => {
   return (
     <div className="container mx-auto m-5">
       <img
-        className="h-[500px] w-full rounded-md "
+        className="h-[500px] w-full rounded-md relative"
         src="https://i.ibb.co/5vc6Ygc/Pngtree-recruitment-background-banner-1037995.jpg"
       ></img>
+      <div className=" flex flex-col gap-2 italic absolute top-[400px] right-[50px] md:top-[320px] md:right-[230px] lg:top-[300px] lg:right-[820px]">
+        <div className="text-6xl font-bold">
+          <span className="text-[]  ">JOB</span> HUNT
+        </div>
+        <h1 className="text-2xl text-">Discover Your Dream Job Today!</h1>
+        <label class="input-group">
+          <input
+            type="text"
+            placeholder="Search Job"
+            class="input input-bordered"
+            id="job"
+          />
+          <button
+            id="btn-apply"
+            class="btn btn-error text-xl font-semibold normal-case text-white"
+          >
+            Search
+          </button>
+        </label>
+      
+      </div>
     </div>
   );
 };
