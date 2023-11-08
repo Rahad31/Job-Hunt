c2ecf46ac71dfdb2c355b0494a226c2434d8e095
Rahad31 <rahad3100@gmail.com>
2023-11-08 18:31:36 +0600

    Home page full responsive

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -5,7 +5,7 @@ const Tabs = () => {
     <div className="flex flex-col justify-center items-center gap-2 my-6">
       <div className="text-4xl font-bold text-[#EAA334]">JOB CATEGORY</div>
       <div className="text-xl">Discover Your Dream Job Today!</div>
-      <div className="flex gap-6">
+      <div className="flex flex-wrap justify-center items-center gap-6">
         <button className="btn btn-error">On Site Job</button>
         <button className="btn btn-error">Remote Job</button>
         <button className="btn btn-error">Hybrid</button>
